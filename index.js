36dccc1ca3b2b10e2ecaaf47749b70c79242d40c
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-17 09:06:09 +0200

    lab

---
+++ index.js
@@ -0,0 +1,9 @@
+var name =prompt("please enter your name");
+var age =prompt("please enter your age");
+var carrer=prompt("please enter your proffosion");
+var info =prompt("can you please tell us about your carrer");
+
+    document.getElementById("name").innerHTML = "welcome  "+ name;
+    document.getElementById("age").innerHTML = " wow you are "+age +"  years old" ;
+    document.getElementById("carrer").innerHTML ="that's great your handsome   " + carrer;
+    document.getElementById("article").innerHTML = info;
