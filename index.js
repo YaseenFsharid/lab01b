3e236e83173c208207be754e7256603de857135f
YaseenFsharid <yaseen.sharid@gmail.com>
2020-02-20 09:41:52 +0200

    rewrite the whole things

--- index.js
+++
@@ -1,9 +0,0 @@
-var name =prompt("please enter your name");
-var age =prompt("please enter your age");
-var carrer=prompt("please enter your proffosion");
-var info =prompt("can you please tell us about your carrer");
-
-    document.getElementById("name").innerHTML = "welcome  "+ name;
-    document.getElementById("age").innerHTML = " wow you are "+age +"  years old" ;
-    document.getElementById("carrer").innerHTML ="that's great your handsome   " + carrer;
-    document.getElementById("article").innerHTML = info;
